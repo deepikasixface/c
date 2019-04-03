27f74829b78d451d313ba86f3991a7729657263f
deepikasixface <47886937+deepikasixface@users.noreply.github.com>
2019-04-03 15:49:30 +0530

    Create set1_1.c

---
+++ set1_1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a>1)
+{
+ printf("positive");
+ }
+ elseif(a==0)
+ {
+ printf("zero");
+ }
+ else
+ {
+ printf("negative");
+ }
+ return 0;
+ }
